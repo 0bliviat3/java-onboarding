595fd167b0bf7bb3c0d65a2dfa214e99f17be995
song-jae-wan <sjo9810@gmail.com>
2023-10-08 21:17:46 +0900

    Problem4 complate

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,17 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder sb = new StringBuilder();
+        for(char val : word.toCharArray()) {
+            if(val >= 'A' && val <= 'Z') {
+                sb.append((char)('Z' + 'A' - val));
+            }else if(val >= 'a' && val <= 'z') {
+                sb.append((char)('z' + 'a' - val));
+            }else {
+                sb.append(val);
+            }
+        }
+        return sb.toString();
     }
+
 }
