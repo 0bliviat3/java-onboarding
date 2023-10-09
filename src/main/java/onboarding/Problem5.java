3b92b768a347bf3250fc07109e8b899867353bd7
song-jae-wan <sjo9810@gmail.com>
2023-10-09 12:44:56 +0900

    Problem5 complate

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,24 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+
+        for(int i = 50000; i >= 50; i/=2) {
+            for(int j = 0; j < 2; j++) {
+                int mod = money/i;
+                answer.add(mod);
+                money -= mod * i;
+                i/=5;
+            }
+            i*=5;
+        }
+
+        answer.add(money%10);
+
         return answer;
     }
 }
